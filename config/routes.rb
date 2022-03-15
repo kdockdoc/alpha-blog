325e5f667f75d050ff7271abca89e477b6c26c20
Reynolds <kdockdoc@gmail.com>
2022-03-15 01:18:37 +0000

    create new post with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcom/index'
+  resources :posts
+  get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'welcom#index'
+  root 'posts#index'
+
+
+ 
 end
